c3118750a8419e468d47927057d9c838bc76bb80
shiroshan <siroshang@gmail.com>
2023-06-26 12:36:27 +0530

    feat: Inject custom style for tenants in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import { TenantService } from '@services/tenant';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    const subdomain = ctx?.req?.headers?.host?.split('.')[0];
+    const tenantId = await TenantService.getTenantIdBySubdomain(subdomain!);
+    const tenant = await TenantService.getTenant(+tenantId);
+
+    return {
+      ...initialProps,
+      styles: <RootStyleLoader theme={tenant.theme} />,
+    };
+  }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
