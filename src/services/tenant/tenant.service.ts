700a280c6e190e95232fb68c7ad2ac1d71199f61
shiroshan <siroshang@gmail.com>
2023-06-30 09:59:14 +0530

    patch: Add get tenant categories service

--- src/services/tenant/tenant.service.ts
+++ src/services/tenant/tenant.service.ts
@@ -1,12 +1,13 @@
 import { ITenant } from '@models/Tenant';
-import { axiosUserInstance } from '../api';
 import { ISubdomain } from '@models/Subdomain';
+import { axiosInstance } from '@services/api/api';
+import { ICategory } from '@models/Category';
 
 export const getTenantIdBySubdomain = async (
   subdomain: string
 ): Promise<number> => {
   try {
-    const { data } = await axiosUserInstance.get<ISubdomain[]>(
+    const { data } = await axiosInstance.get<ISubdomain[]>(
       `subdomains?subdomain=${subdomain}`
     );
     if (data[0]) {
@@ -20,10 +21,19 @@ export const getTenantIdBySubdomain = async (
 };
 
 export const getTenant = async (id: number): Promise<ITenant> => {
-
   try {
-    const { data } = await axiosUserInstance.get<ITenant>(`tenants/${id}`);
+    const { data } = await axiosInstance.get<ITenant>(`tenants/${id}`);
     return data;
+  } catch (err) {
+    console.log('error', err);
+    throw err;
+  }
+};
+
+export const getTenantCategories = async (id: number): Promise<ICategory[]> => {
+  try {
+    const { data } = await axiosInstance.get<ITenant>(`tenants/${id}`);
+    return data.categories;
   } catch (err) {
     console.log('error', err);
     throw err;
