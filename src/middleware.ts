64251bc643365ccca327096da4c2e6ea106b0751
shiroshan <siroshang@gmail.com>
2023-06-30 09:54:12 +0530

    patch: Add public file access from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,8 +13,14 @@ export const config = {
   ],
 };
 
+// RegExp for public files
+const PUBLIC_FILE = /\.(.*)$/; // Files
+
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
+
+  // Skip public files
+  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   const hostname = req.headers.get('host');
@@ -27,7 +33,7 @@ export default async function middleware(req: NextRequest) {
   if (path.includes('_sites/')) {
     path = path.replace('_sites/', '');
   }
-  
+
   // rewrite everything else to `/_sites/[site] dynamic route
   return NextResponse.rewrite(
     new URL(`/_sites/${currentHost}${path}`, req.url)
