5b4a98081d8ade1b207624da1c2ae2bb786f7104
shiroshan <siroshang@gmail.com>
2023-06-30 09:59:49 +0530

    patch: Implement get category color function

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,10 +1,25 @@
 export const categories = [
-    {
-      title: "men's clothing",
-      link: 'mens-clothing',
-    },
-    {
-      title: "women's clothing",
-      link: 'womens-clothing',
-    },
-  ];
+  {
+    title: "men's clothing",
+    link: 'mens-clothing',
+  },
+  {
+    title: "women's clothing",
+    link: 'womens-clothing',
+  },
+];
+
+export const getCategoryColor = (category: string) => {
+  if (category === "men's clothing") {
+    return 'bg-category-men';
+  }
+  if (category === "women's clothing") {
+    return 'bg-category-women';
+  }
+  if (category === 'electronics') {
+    return 'bg-category-electronics';
+  }
+  if (category === 'jewelery') {
+    return 'bg-category-jewelery';
+  }
+};
